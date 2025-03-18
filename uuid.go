2bcf225fa19026887d4a27f8025a2a4ea0901f25
julianto <julianto@lumoshive.com>
2025-03-18 19:45:06 +0700

    add full uuid func

--- uuid.go
+++ uuid.go
@@ -1,9 +1,14 @@
 package tools
 
 import (
+	"github.com/google/uuid"
 	"github.com/lithammer/shortuuid/v4"
 )
 
 func ShortUUID() string {
 	return shortuuid.New()
+}
+
+func NewUUID() string {
+	return uuid.New().String()
 }
