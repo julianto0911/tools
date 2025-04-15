8565884aea3c8f68cbc90b14de40cf9dde7789ec
julianto <julianto@lumoshive.com>
2025-04-15 15:34:35 +0700

    add mailer func

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.24.0
 	golang.org/x/text v0.16.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/driver/postgres v1.5.9
@@ -77,5 +78,6 @@ require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ mailer.go
@@ -0,0 +1,105 @@
+package tools
+
+import (
+	"log"
+
+	"gopkg.in/gomail.v2"
+)
+
+func NewMailer(sender string, to []string, cc []mailCC, subject string, attachments []string, config mailConfig) Mailer {
+	return &mailer{
+		Sender:      sender,
+		To:          to,
+		CC:          cc,
+		Subject:     subject,
+		Attachments: attachments,
+		Config:      config,
+	}
+}
+
+type Mailer interface {
+	SendMail(doLog bool) error
+}
+
+type mailer struct {
+	Sender      string
+	To          []string
+	CC          []mailCC
+	Subject     string
+	Attachments []string
+	Config      mailConfig
+	Body        mailBody
+}
+
+type mailCC struct {
+	Email string
+	Name  string
+}
+
+type mailBody struct {
+	Content     string
+	ContentType string
+}
+
+type mailConfig struct {
+	HOST          string
+	PORT          int
+	AUTH_EMAIL    string
+	AUTH_PASSWORD string
+}
+
+func (m *mailer) doMailLog(doLog bool, err error) {
+	if !doLog {
+		return
+	}
+
+	if err != nil {
+		log.Println("====Email Error====")
+	}
+
+	log.Println("From :", m.Sender)
+	log.Println("To :", m.To)
+	log.Println("CC :", m.CC)
+	log.Println("Subject :", m.Subject)
+	log.Println("Attachment :", m.Attachments)
+
+	if err != nil {
+		log.Println("Error :", err)
+	}
+}
+
+func (m *mailer) SendMail(doLog bool) error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", m.Sender)
+
+	for _, to := range m.To {
+		mailer.SetHeader("To", to)
+	}
+
+	for _, cc := range m.CC {
+		mailer.SetAddressHeader("Cc", cc.Email, cc.Name)
+	}
+
+	mailer.SetHeader("Subject", m.Subject)
+	mailer.SetBody(m.Body.ContentType, m.Body.Content)
+
+	for _, attachment := range m.Attachments {
+		mailer.Attach(attachment)
+	}
+
+	dialer := gomail.NewDialer(
+		m.Config.HOST,
+		m.Config.PORT,
+		m.Config.AUTH_EMAIL,
+		m.Config.AUTH_PASSWORD,
+	)
+
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		m.doMailLog(doLog, err)
+		return err
+	}
+
+	m.doMailLog(doLog, nil)
+	return nil
+}
